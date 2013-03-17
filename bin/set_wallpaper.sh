2446aa33eb8d37e3072ff72f2b61365e928ad80c
morphizer <morphizer@gmail.com>
2013-03-17 22:49:50 +1100

    Set wallpaper to scale

--- bin/set_wallpaper.sh
+++ bin/set_wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
     find /media/moviesmusic/Pictures/Wallpapers -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
-        shuf -n1 -z | xargs -0 feh --bg-scale
+        shuf -n1 -z | xargs -0 feh --bg-fill
     sleep 15m
 done
 
